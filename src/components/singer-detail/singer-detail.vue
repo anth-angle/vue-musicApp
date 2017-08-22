b9b33406bee206ade5585a1f0b202ebbed10cf6a
angle <cizetoeasy@gmail.com>
2017-08-23 00:08:09 +0800

    get singerdetails from api

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,15 +6,52 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import {getSingerDetail} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
   created () {
-    console.log(this.singer)
+    this._getDetails()
+  },
+  data () {
+    return {
+      songs: []
+    }
   },
   computed: {
     ...mapGetters([
       'singer'
     ])
+  },
+  methods: {
+    _getDetails () {
+      if (!this.singer.id) {
+        this.$router.push({
+          path: '/singer'
+        })
+        return
+      }
+      console.log(this.singer)
+      getSingerDetail(this.singer.id)
+      .then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
   }
 }
 </script>
